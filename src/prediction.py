19cb0f61641b83ddf6d7a0f71ec433eda5a2427c
HalvarKelm <halvar.kelm@gmail.com>
2021-05-05 11:58:55 +0200

    Fixed wrong output paths. Added logs and comments.

--- src/prediction.py
+++ src/prediction.py
@@ -11,20 +11,32 @@ def get_corresponding_path(input_path, input_directory, output_directory):
     input-directory-beginning of the input path with the 
     output-directory-beginning
 
-    :param input_path: the path in the input directory
+    :param input_path: the directory path in the input directory
     :param input_directory: the input directory
     :param output_directory: the output directory
 
-    :returns: the corresponding output path in the output directory
+    :returns: the corresponding output directory path in the output directory
     """
-    parts = list(Path(input_path).parts)
+    # make sure the given input path is a directory
+    parts = list(Path(input_path).parts) if Path(input_path).is_dir else Path(input_path).is_dir[:-1]
+    
+    # get the input path parts
     input_parts = list(Path(input_directory).parts)
+    
+    # get the output path parts
     output_parts = list(Path(output_directory).parts)
+    
+    # remove the input path parts
     parts_without_input_parts = [part for (index, part) in enumerate(parts) if len(input_parts) <= index or input_parts[index] != part]
+    
+    # add the output path parts
     output_parts += parts_without_input_parts
+
+    # create the output path
     output_path = output_parts[0]
     for part in output_parts[1:]:
         output_path = os.path.join(output_path, part)
+
     return output_path
 
 if __name__ == "__main__":
@@ -58,18 +70,19 @@ if __name__ == "__main__":
         for dirpath, dirname, filenames in os.walk(input_directory):
             for filename in filenames:
                 if ".png" in filename:
-                # example read input image
+                    # example read input image
                     input_file_path = os.path.join(dirpath, filename)
                     input_img = cv2.imread(input_file_path)
                     # example operation on image
                     inverted_input_img = cv2.bitwise_not(input_img)
                     # example get corresponding output path
-                    output_file_path = os.path.join(get_corresponding_path(input_file_path, input_directory, output_directory), filename)
+                    output_file_path = os.path.join(get_corresponding_path(dirpath, input_directory, output_directory), filename)
                     # example create the output subfolders if they not exist
                     if not os.path.exists(os.path.dirname(output_file_path)):
                         os.makedirs(os.path.dirname(output_file_path))
                     # example writing of image
                     cv2.imwrite(output_file_path, inverted_input_img)
+        print("Domain transformation successfully completed. The transformed images can now be found in the host output directory.")
         quit()
 
     # Mandatory: TODO Import and call landmark detection code and create a json file at the output file path
@@ -96,10 +109,11 @@ if __name__ == "__main__":
                     }]
                 }
                 # example get corresponding output path
-                output_file_path = os.path.join(get_corresponding_path(input_file_path, input_directory, output_directory), Path(filename).stem + ".json")
+                output_file_path = os.path.join(get_corresponding_path(dirpath, input_directory, output_directory), Path(filename).stem + ".json")
                 # example create the output subfolders if they not exist
                 if not os.path.exists(os.path.dirname(output_file_path)):
                     os.makedirs(os.path.dirname(output_file_path))
                 # example write the json file
                 output_file = open(output_file_path, "w")
                 json.dump(labels, output_file, indent=4)
+    print("Landmark detection successfully completed. The json files with the extracted points can now be found in the host output directory.")
